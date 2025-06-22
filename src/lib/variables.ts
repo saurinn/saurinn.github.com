87953e26fdbe38b1ad46596187e06c24da4703b9
Manuel Valdez <saurinn@protonmail.com>
2025-06-21 21:29:18 -0400

    basic updates

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -2,9 +2,9 @@
 
 export const GLOBAL = {
   // Site metadata
-  username: "Zaggonaut",
-  rootUrl: "https://zaggonaut.dev",
-  shortDescription: "Retro-Inspired Theme &<br /> Built for Astro",
+  username: "Manuel Valdez",
+  rootUrl: "https://saurinn.github.com",
+  shortDescription: "Web application security blog &<br> Bug bounty hunting stuff",
   longDescription: "Zaggonaut is a retro-inspired theme for Astro, built using TypeScript, TailwindCSS, and Astro.",
   
   // Social media links
