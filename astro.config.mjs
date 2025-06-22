ecddad44dd2b398c4c0781ae865df560f5d0ca9a
Manuel Valdez <saurinn@protonmail.com>
2025-06-21 23:03:41 -0400

    actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://saurinn.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
