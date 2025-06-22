e3ff648c3423cc771d22f37b143b7fd7288a61ca
Manuel Valdez <saurinn@protonmail.com>
2025-06-22 11:45:01 -0400

    Update featured.ts

--- src/lib/featured.ts
+++ src/lib/featured.ts
@@ -1,32 +1,7 @@
 import type { ArticleFrontmatter, ProjectFrontmatter } from "./types";
 import { getShortDescription, processContentInDir } from "./utils";
 
-export const featuredProjects = (
-  await processContentInDir<ProjectFrontmatter, ProjectFrontmatter>(
-    "projects",
-    (data) => {
-      const shortDescription = getShortDescription(
-        data.frontmatter.description,
-      );
-      return {
-        title: data.frontmatter.title,
-        description: shortDescription,
-        tags: data.frontmatter.tags,
-        githubUrl: data.frontmatter.githubUrl,
-        liveUrl: data.frontmatter.liveUrl,
-        featured: data.frontmatter.featured,
-        timestamp: data.frontmatter.timestamp,
-        filename: `/projects/${data.frontmatter.filename}`,
-      };
-    },
-  )
-)
-  .filter((project) => project.featured)
-  .sort((a, b) => {
-    const dateA = new Date(a.timestamp);
-    const dateB = new Date(b.timestamp);
-    return dateB.getTime() - dateA.getTime();
-  });
+
 
 export const featuredArticles = (
     await processContentInDir<ArticleFrontmatter, ArticleFrontmatter>(
@@ -47,9 +22,4 @@ export const featuredArticles = (
       },
     )
   )
-    .filter((project) => project.featured)
-    .sort((a, b) => {
-      const dateA = new Date(a.timestamp);
-      const dateB = new Date(b.timestamp);
-      return dateB.getTime() - dateA.getTime();
-    });
+    ;
